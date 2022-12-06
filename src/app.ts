8b66e1aa9f45fc8779a2a53ef9d51d238f19ccf7
gpmarchi <gustavomarchi@gmail.com>
2022-12-06 14:26:09 -0300

    feat: add user simple crud operations

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,86 @@
 import express, { Router } from 'express';
 import ServerlessHttp from 'serverless-http';
 
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 const app = express();
 
 app.use(express.json());
 
 const routes = Router();
 
-routes.get('/test', (request, response) => response.json({ message: 'hello' }));
+routes.post('/users', async (request, response) => {
+  const { email, password, first_name, last_name, birthdate, phone } =
+    request.body;
+
+  const user = await prisma.users.create({
+    data: {
+      email,
+      password,
+      birthdate: new Date(birthdate),
+      first_name,
+      last_name,
+      phone,
+    },
+  });
+
+  response.json(user);
+});
+
+routes.patch('/users/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const { email, password, first_name, last_name, birthdate, phone } =
+    request.body;
+
+  const user = await prisma.users.update({
+    where: {
+      id,
+    },
+    data: {
+      email,
+      password,
+      birthdate: new Date(birthdate),
+      first_name,
+      last_name,
+      phone,
+    },
+  });
+
+  response.json(user);
+});
+
+routes.get('/users', async (request, response) => {
+  const users = await prisma.users.findMany();
+
+  response.json(users);
+});
+
+routes.get('/users/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const users = await prisma.users.findFirst({
+    where: {
+      id,
+    },
+  });
+
+  response.json(users);
+});
+
+routes.delete('/users/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const users = await prisma.users.delete({
+    where: {
+      id,
+    },
+  });
+
+  response.json(users);
+});
 
 app.use(routes);
 
